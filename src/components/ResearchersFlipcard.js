bcdb0f2c70e27dde893ff5d070bf5f837863a44d
teodorasorescu <tsorescu@gmail.com>
2024-01-25 13:32:51 +0200

    fixed bugs

--- src/components/ResearchersFlipcard.js
+++ src/components/ResearchersFlipcard.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import '../styling/ResearchersFlipcard.css';
 import colorResearchers from '../constants/colorResearchers';
+import SwipeIcon from '../images/hand.png';
 
 const ResearchersFlipcard = () => {
 	return (
-		<div className='boxContainer'>
-			{colorResearchers.map((c, i) => {
-				return (
-					<div className='boxItem' key={i}>
-						<div className='flipBox'>
-							<div
-								className='flipBoxFront textCenter'
-								style={{ backgroundImage: `${c.image}` }}
-							>
-								<div className='nameContainer'>
-									<h3 className='flipBoxHeader'>{c.researcher}</h3>
+		<div>
+			<div className='boxContainer'>
+				{colorResearchers.map((c, i) => {
+					return (
+						<div className='boxItem' key={i}>
+							<div className='flipBox'>
+								<div
+									className='flipBoxFront textCenter'
+									style={{ backgroundImage: `${c.image}` }}
+								>
+									<div className='nameContainer'>
+										<h3 className='flipBoxHeader'>{c.researcher}</h3>
+									</div>
 								</div>
-							</div>
-							<div
-								className='flipBoxBack textCenter'
-								style={{ backgroundColor: 'rgba(248, 221, 170, 0.3)' }}
-							>
-								<div className='inner'>
-									<p>{c.desc}</p>
+								<div
+									className='flipBoxBack textCenter'
+									style={{ backgroundColor: 'rgba(248, 221, 170, 0.3)' }}
+								>
+									<div className='inner'>
+										<p>{c.desc}</p>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
+			<div className='swipeContainer'>
+				<img src={SwipeIcon} alt='swipeIcon' />
+				<p>Glișați spre stânga.</p>
+			</div>
 		</div>
 	);
 };
