90bd18e94abc77ba0cd5911943027377ffb49df4
teodorasorescu <tsorescu@gmail.com>
2024-08-01 18:58:56 +0300

    removed node-saas

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -2,26 +2,21 @@ import React from 'react';
 import '../styling/showcase.css';
 export const Showcase = ({ jpg }) => {
 	return (
-		<div>
-			<div className='container'>
-				<h1 className='text'>Inspirație</h1>
+		<section className='section-houses'>
+			<div className='houses-showcase'>
+				{jpg.map((el) => {
+					return (
+						<img
+							className='photo'
+							src={el.image}
+							alt='Inspiratie Tablouri Gradient Design Interior Contemporan'
+							key={Math.random() * 101}
+							width={'40%'}
+						/>
+					);
+				})}
 			</div>
-			<section className='section-houses'>
-				<div className='houses-showcase'>
-					{jpg.map((el) => {
-						return (
-							<img
-								className='photo'
-								src={el.image}
-								alt='Inspiratie Tablouri Gradient Design Interior Contemporan'
-								key={Math.random() * 101}
-								width={'40%'}
-							/>
-						);
-					})}
-				</div>
-			</section>
-		</div>
+		</section>
 	);
 };
 
