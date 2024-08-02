2f51d387b28052450fb8cb65c6f35269930aabaa
teodorasorescu <tsorescu@gmail.com>
2024-08-02 17:05:19 +0300

    changed inspo title

--- src/components/Inspiration.jsx
+++ src/components/Inspiration.jsx
@@ -4,6 +4,7 @@ import classes from '../styling/inspiration.module.scss';
 export const Inspiration = () => {
 	return (
 		<div className={classes.container}>
+			<h1 className='text'>Inspirație</h1>
 			<Showcase jpg={inspoData} />{' '}
 		</div>
 	);
