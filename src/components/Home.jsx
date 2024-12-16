66a93515834f46b77d251c44e005544970b638e2
teodorasorescu <tsorescu@gmail.com>
2024-12-16 12:37:12 +0200

    removed countdown

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,14 +9,14 @@ import ColorsChoice from './ColorsChoice';
 import PostersHomeLoading from './features/posters/PostersHomeLoading';
 import StickersHomeLoading from './features/stickers/StickersHomeLoading';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import Countdown from './features/countdown/Countdown';
+import Button from '@mui/material/Button';
 
 const Home = () => {
 	const smartphoneScreen = useMediaQuery('(max-width:1024px)');
 
 	return (
 		<div className={styles.container}>
-			{/* <div className={styles.introductionImgContainer}>
+			<div className={styles.introductionImgContainer}>
 				{
 					<div className={styles.introductionContainer}>
 						{!smartphoneScreen && (
@@ -32,30 +32,12 @@ const Home = () => {
 						</div>
 					</div>
 				}
-			</div> */}
-			{/* {!smartphoneScreen && (
-				<div className={styles.introductionContainer}>
-					<h1 className={styles.title}>
-						Embrace the Colors of Your Being and Feelings
-					</h1>
-					<h3 className={styles.subtitle}>
-						New Canvas Art Prints and Frames Available Now <br />
-						Limited Edition
-					</h3>
-					<div className={styles.buttonContainer}>
-						<a href='/canvas-art-prints'>
-							<Button className={styles.button}>GET YOURS</Button>
-						</a>
-					</div>
-				</div>
-			)} */}
-			<Countdown targetDate={'2024-12-15T23:59:59'} />
+			</div>
+			<PostersHomeLoading /> <Benefits />
 			<div className={styles.text}>
 				<h3>TABLOURI PERSONALIZATE</h3>
 			</div>
 			<Flipcard />
-			<Benefits />
-			<PostersHomeLoading />
 			<StickersHomeLoading />
 			<VisualImages />
 			<ColorsChoice />
