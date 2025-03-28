2382cde588df5e8204f1858fb62c96f3860082f1
teodorasorescu <tsorescu@gmail.com>
2025-03-28 14:12:37 +0200

    change buttons

--- src/components/VisiualImages.jsx
+++ src/components/VisiualImages.jsx
@@ -1,15 +1,21 @@
 import styles from '../styling/visual.images.module.scss';
+import Button from './ui/button/Button';
 
 const VisualImages = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.imgContainer}>
 				<div className={styles.firstContainer}></div>
-				<a href='/customized-canvas-posters'>Custom Your Canvas Poster</a>
+
+				<a href='/customized-canvas-posters'>
+					<Button msg={'Custom Your Canvas Poster'} />
+				</a>
 			</div>
 			<div className={styles.imgContainer}>
 				<div className={styles.secondContainer}></div>
-				<a href='/journal'>Journal</a>
+				<a href='/journal'>
+					<Button msg={'Journal'} />
+				</a>
 			</div>
 		</div>
 	);
