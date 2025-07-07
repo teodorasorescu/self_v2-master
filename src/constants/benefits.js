22d6ab8b644231290da261d8b76f196cb5e97bbf
teodorasorescu <tsorescu@gmail.com>
2025-07-07 20:46:53 +0300

    benefits added

--- src/constants/benefits.js
+++ src/constants/benefits.js
@@ -1,5 +1,13 @@
 const benefits = [
 	{
+		title: 'EASY RETURNS',
+		desc: "14-Day Easy Returns and if damaged we'll replace it for free.",
+	},
+	{
+		title: 'FAST SAMEDAY DELIVERY',
+		desc: 'Fast Shipping with Sameday to Romania, Ungary & Bulgaria.',
+	},
+	{
 		title: 'UNIQUE DESIGNS',
 		desc: 'Curated store of expressive designs which support independent artists.',
 	},
@@ -10,10 +18,6 @@ const benefits = [
 	{
 		title: 'SAFE & SECURE CARD PAYMENTS',
 		desc: 'Powered by Stripe you can pay directly with card or through Apple / Google Pay.',
-	},
-	{
-		title: 'EASY RETURNS',
-		desc: '14-Day Easy Returns and if damaged we\'ll replace it for free.',
 	},
 ];
 
