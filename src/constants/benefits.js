4abe1c3257901c5648246c15383d882ba0d68100
teodorasorescu <tsorescu@gmail.com>
2025-07-07 21:41:21 +0300

    benefits

--- src/constants/benefits.js
+++ src/constants/benefits.js
@@ -1,5 +1,9 @@
 const benefits = [
 	{
+		title: '100% COTTON CANVAS & PRINT QUALITY',
+		desc: 'We use 100% cotton canvas, a material that guarantees quality and durability of colors.',
+	},
+	{
 		title: 'EASY RETURNS',
 		desc: "14-Day Easy Returns and if damaged we'll replace it for free.",
 	},
@@ -11,10 +15,7 @@ const benefits = [
 		title: 'UNIQUE DESIGNS',
 		desc: 'Curated store of expressive designs which support independent artists.',
 	},
-	{
-		title: '100% COTTON CANVAS & PRINT QUALITY',
-		desc: 'We use 100% cotton canvas, a material that guarantees quality and durability of colors.',
-	},
+
 	{
 		title: 'SAFE & SECURE CARD PAYMENTS',
 		desc: 'Powered by Stripe you can pay directly with card or through Apple / Google Pay.',
