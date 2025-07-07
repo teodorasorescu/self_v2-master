70a749ee98f62654802d94b3eb8c352d6e301886
teodorasorescu <tsorescu@gmail.com>
2025-07-07 20:47:30 +0300

    modified benefits

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -12,7 +12,7 @@ const Benefits = () => {
 
 		speed: 500,
 		slidesToShow: isMobile ? 1.1 : benefits.length, // Show all on larger screens, one per slide on mobile
-		slidesToScroll: 1,
+		slidesToScroll: 2,
 		arrows: false, // Optional: hide arrows on mobile for cleaner UI
 	};
 
