d3920b1583d7d1e30d491024cf4486c36d340e21
teodorasorescu <61358276+teodorasorescu@users.noreply.github.com>
2025-07-08 11:53:46 +0300

    Update Reviews.jsx

--- src/components/features/reviews/Reviews.jsx
+++ src/components/features/reviews/Reviews.jsx
@@ -12,8 +12,8 @@ const Reviews = () => {
 			<ReviewCard
 				name='Fuchila Patricia'
 				rating={5}
-				review='SERIOUSNESS, QUALITY and DEDICATION can be seen with the naked eye in the painting!
-        The colors and their appearance definitely blew me away and what such a personalized painting can convey is incredible!
+				review='SERIOUSNESS, QUALITY and DEDICATION can be seen with the naked eye in the poster!
+        The colors and their appearance definitely blew me away and what such a personalized poster can convey is incredible!
         Thank you from the bottom of my heart and I am very happy with the purchase 🤍🥺'
 			/>
 		</div>
