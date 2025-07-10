50eb719d88405242b9ae35b7e8a574395445848c
teodorasorescu <tsorescu@gmail.com>
2025-07-10 18:32:08 +0300

    added cdn

--- src/constants/links.js
+++ src/constants/links.js
@@ -2,5 +2,4 @@ export const BACKEND_PATH = 'https://self-be.onrender.com';
 
 //export const BACKEND_PATH = 'http://localhost:8080';
 export const BACKEND_ERROR_MESSAGE = 'Error has occured. Try again later.';
-export const S3_BUCKET =
-	'https://myselfpostersbucket11.s3.eu-central-1.amazonaws.com';
+export const S3_BUCKET = 'https://d2inw1jvxszbn9.cloudfront.net';
