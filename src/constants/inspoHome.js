e529506c2053e43b03ee732dcbc39329ce7794bc
teodorasorescu <tsorescu@gmail.com>
2025-09-04 21:07:07 +0300

    changed inspiration

--- src/constants/inspoHome.js
+++ src/constants/inspoHome.js
@@ -1,107 +1,75 @@
 import { S3_BUCKET } from './links';
 
-const inspo16 = S3_BUCKET + '/diningarea.webp';
-const inspo17 = S3_BUCKET + '/livingarea.webp';
-const inspo4 = S3_BUCKET + '/inspo4.webp';
-const inspo8 = S3_BUCKET + '/inspo8.webp';
-const inspo14 = S3_BUCKET + '/inspo14.webp';
-const inspo15 = S3_BUCKET + '/inspo15.webp';
-const inspo20 = S3_BUCKET + '/inspo21.webp';
-const inspo21 = S3_BUCKET + '/3.webp';
-const inspo22 = S3_BUCKET + '/WaterShowcase1.webp';
-const inspo23 = S3_BUCKET + '/WaterShowcase2.webp';
-const inspo24 = S3_BUCKET + '/2.webp';
-const inspo25 = S3_BUCKET + '/1.webp';
-const inspo26 = S3_BUCKET + '/inspo22.webp';
-const inspo27 = S3_BUCKET + '/inspo24.webp';
-const inspo28 = S3_BUCKET + '/inspo23.webp';
+const inspo1 = S3_BUCKET + '/inspo21.webp';
+
+const inspo2 = S3_BUCKET + '/inspo24.webp';
+const inspo3 = S3_BUCKET + '/inspo23.webp';
+const inspo4 = S3_BUCKET + '/coffee.webp';
+const inspo5 = S3_BUCKET + '/table2.webp';
+const inspo7 = S3_BUCKET + '/table3.webp';
+const inspo8 = S3_BUCKET + '/analiana.webp';
+const inspo6 = S3_BUCKET + '/zato5.webp';
+const inspo9 = S3_BUCKET + '/frontPage.webp';
 
 const homeImg1 = S3_BUCKET + '/leleCafe.webp';
 const homeImg2 = S3_BUCKET + '/homePageSmartphone.jpg';
 
 export const inspoHome = [
 	{
-		image: `url(${inspo28})`,
+		image: `url(${inspo3})`,
 		heading: 'Slide Four',
 		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
 	},
 	{
-		image: `url(${inspo27})`,
+		image: `url(${inspo9})`,
 		heading: 'Slide Four',
 		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
 	},
 	{
-		image: `url(${inspo26})`,
+		image: `url(${inspo2})`,
 		heading: 'Slide Four',
 		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
 	},
-	{
-		image: `url(${inspo25})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-	{
-		image: `url(${inspo24})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-	{
-		image: `url(${inspo21})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-	{
-		image: `url(${inspo22})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-	{
-		image: `url(${inspo23})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-	{
-		image: `url(${inspo20})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-
-	{
-		image: `url(${inspo16})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-
-	{
-		image: `url(${inspo15})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-	{
-		image: `url(${inspo17})`,
-		heading: 'Slide Four',
-		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-	{
-		image: `url(${inspo14})`,
-		heading: 'Slide One',
-		desc: 'This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-
-	{
-		image: `url(${inspo15})`,
-		heading: 'Slide One',
-		desc: 'This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
-	},
-
 	{
 		image: `url(${inspo4})`,
-		heading: 'Slide One',
-		desc: 'This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
+		heading: 'Slide Four',
+		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
+	},
+
+	{
+		image: `url(${inspo5})`,
+		heading: 'Slide Four',
+		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
+	},
+
+	{
+		image: `url(${inspo7})`,
+		heading: 'Slide Four',
+		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
 	},
 	{
 		image: `url(${inspo8})`,
-		heading: 'Slide Five',
+		heading: 'Slide Four',
+		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
+	},
+	{
+		image: `url(${homeImg1})`,
+		heading: 'Slide Four',
+		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
+	},
+	{
+		image: `url(${inspo6})`,
+		heading: 'Slide Four',
+		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
+	},
+	{
+		image: `url(${homeImg2})`,
+		heading: 'Slide Four',
+		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
+	},
+	{
+		image: `url(${inspo1})`,
+		heading: 'Slide Four',
 		desc: 'This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.',
 	},
 ];
